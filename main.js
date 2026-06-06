7479a0e43b67f9a62330eb0719b4f71ef593c9c9
Rufus Gordon-Heywood <rufus.gh@icloud.com>
2026-06-06 21:55:21 +1000

    the matrix now transforms the gridlines as well as the vector

--- main.js
+++ main.js
@@ -73,19 +73,19 @@ function drawVector(x, y) {
 }
 
 function drawGrid() {
-    // vertical lines
-    for (let x=Math.trunc(canvas.width / 4);x<canvas.width;x+=Math.trunc(canvas.width / 70)) {
-      drawLine(x,0,x,canvas.height);
+    const gridSpacing = Math.trunc(canvas.width / 70);
+    const gridRange = 30;
+    
+    for (let x = -gridRange; x <= gridRange; x++) {
+      let [x1, y1] = canvasCoords(x, -gridRange);
+      let [x2, y2] = canvasCoords(x, gridRange);
+      drawLine(x1, y1, x2, y2);
     }
-    for (let y=Math.trunc(canvas.height / 4);y<canvas.height;y+=Math.trunc(canvas.width / 70)) {
-      drawLine(0,y,canvas.width,y);
-    }
-
-    for (let x=Math.trunc(canvas.width / 4);x>0;x-=Math.trunc(canvas.width / 70)) {
-      drawLine(x,0,x,canvas.height);
-    }
-    for (let y=Math.trunc(canvas.height / 4);y>0;y-=Math.trunc(canvas.width / 70)) {
-      drawLine(0,y,canvas.width,y);
+    
+    for (let y = -gridRange; y <= gridRange; y++) {
+      let [x1, y1] = canvasCoords(-gridRange, y);
+      let [x2, y2] = canvasCoords(gridRange, y);
+      drawLine(x1, y1, x2, y2);
     }
 
     ctx.beginPath();
@@ -95,8 +95,13 @@ function drawGrid() {
     ctx.fill();
     ctx.stroke();
 
-    drawLine(0, Math.trunc(canvas.height / 4), canvas.width, Math.trunc(canvas.height / 4), 5, "white");
-    drawLine(Math.trunc(canvas.width / 4), 0, Math.trunc(canvas.width / 4), canvas.height, 5, "white");
+    let [axisX1, axisY1] = canvasCoords(-gridRange, 0);
+    let [axisX2, axisY2] = canvasCoords(gridRange, 0);
+    drawLine(axisX1, axisY1, axisX2, axisY2, 5, "white");
+    
+    let [axisX3, axisY3] = canvasCoords(0, -gridRange);
+    let [axisX4, axisY4] = canvasCoords(0, gridRange);
+    drawLine(axisX3, axisY3, axisX4, axisY4, 5, "white");
 }
 
 function draw() {
